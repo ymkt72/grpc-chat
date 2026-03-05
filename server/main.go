f939062154b51830d426b1673d447639e2eb8fa7
yuma kitsu <kitsu@yumanoMac-mini.local>
2026-03-05 23:29:18 +0900

    update

--- server/main.go
+++ server/main.go
@@ -1,5 +1,30 @@
-package server
+package main
+
+import (
+	"context"
+	"grpcchat/gen/gen"
+	"log"
+)
+
+type ChatServer struct {
+}
+
+func (c *ChatServer) Chat(context.Context, *gen.ClientMessage) (*gen.ServerMessage, error) {
+	serverMessage := gen.ServerMessage{
+		From:      "userA",
+		Text:      "you are dumb!",
+		Timestamp: nil,
+	}
+	return &serverMessage, nil
+}
 
 func main() {
-
+	s := ChatServer{}
+	var ctx context.Context
+	clientMessage := gen.ClientMessage{Username: "test", Text: "hello"}
+	serverMessage, err := s.Chat(ctx, &clientMessage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	println(serverMessage.Text)
 }
